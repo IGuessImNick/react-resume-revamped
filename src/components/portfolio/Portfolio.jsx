ea4d82fdd251ee40589da79deda54fb0de86b365
Nicholas Skinner <njskinner@ucdavis.edu>
2022-12-19 11:11:47 -0800

    finished testimonial section, installed Swiperjs for pagination scroll

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,14 +7,83 @@ import Img4 from '../../assets/portfolio4.jpg'
 import Img5 from '../../assets/portfolio5.png'
 import Img6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: Img1,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: Img2,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: Img3,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: Img4,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: Img5,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: Img6,
+    title: 'Here is the title of the project',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  }
+]
+
+
 const Portfolio = () => {
+
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
+
+        {/* datamap method -> wrap inside javascript {} and have element inside return() statement */}
+
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-img">
+                  <img src={image} alt="" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target="_blank" rel="noreferrer">Github</a>
+                  <a href={demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+
+
+
+        {/* <article className="portfolio__item">
           <div className="portfolio__item-img">
             <img src={Img1} alt="" />
           </div>
@@ -73,7 +142,7 @@ const Portfolio = () => {
             <a href="https://github.com/" className='btn' target="_blank" rel="noreferrer">Github</a>
             <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
           </div>
-        </article>
+        </article> */}
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,72 @@
 import React from 'react'
 import "./testimonials.css"
+import Avatar1 from '../../assets/avatar1.jpg'
+import Avatar2 from '../../assets/avatar2.jpg'
+import Avatar3 from '../../assets/avatar3.jpg'
+import Avatar4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: Avatar1,
+    name: 'Some Dude',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo sit adipisci optio! Tempore eligendi quia minus autem excepturi optio veniam iste facilis, maxime voluptate consequatur repellat dolorum eaque dolor?'
+  },
+  {
+    avatar: Avatar2,
+    name: 'Some Dude',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo sit adipisci optio! Tempore eligendi quia minus autem excepturi optio veniam iste facilis, maxime voluptate consequatur repellat dolorum eaque dolor?'
+  },
+  {
+    avatar: Avatar3,
+    name: 'Some Dude',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo sit adipisci optio! Tempore eligendi quia minus autem excepturi optio veniam iste facilis, maxime voluptate consequatur repellat dolorum eaque dolor?'
+  },
+  {
+    avatar: Avatar4,
+    name: 'Some Dude',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo sit adipisci optio! Tempore eligendi quia minus autem excepturi optio veniam iste facilis, maxime voluptate consequatur repellat dolorum eaque dolor?'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonialss'>
-      testimonials
-    </section>
+      <section id='testimonialss'>
+        <h5>Review from clients</h5>
+        <h2>Testimonials</h2>
+
+        <Swiper className="container testimonials__container"
+        // install Swiper modules
+        modules={[ Pagination ]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        >
+          {
+            data.map(({avatar, name, review}, index) => {
+              return (
+                <SwiperSlide key={index} className="testimonial">
+                  <div className="client__avatar">
+                    <img src={avatar} alt="" />
+                  </div>
+                  <h5 className='client__name'>{name}</h5>
+                  <small className="client__review">
+                    {review}
+                  </small>
+                </SwiperSlide>
+              )
+            })
+          }
+        </Swiper>
+      </section>
   )
 }
 
