8dbd9b2c408990c8a956a05529ab6e4040fca689
Nicholas Skinner <njskinner@ucdavis.edu>
2022-12-19 15:26:06 -0800

    finished contact section, linked emailjs to form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import "./contact.css"
+import {HiOutlineMail} from 'react-icons/hi'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fdoxis9', 'template_gl30yt8', form.current, '49ULZv6GKMsyZiiF7')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
-      contact
+      <h5>Get In Touch</h5>
+      <h2>Contact</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>imnickskinner@gmail.com</h5>
+            <a href="mailto:imnickskinner@gmail.com" target="_blank" rel="noreferrer">Send an email</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='email' placeholder='Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
